6337a4a5a99144f375c03de7ba945de74f827a1c
Marcelo Junior <marcelojunior.07@live.com>
2025-12-02 13:25:10 -0300

    ajusted layout dashboard

--- src/pages/AccountsPayable.tsx
+++ src/pages/AccountsPayable.tsx
@@ -271,7 +271,7 @@ const AccountsPayable = () => {
       </div>
 
       {/* Dashboard Cards */}
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-7">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <Card
           className={`cursor-pointer transition-all hover:shadow-md ${activeFilter === 'open' ? 'ring-2 ring-primary' : ''}`}
           onClick={() => handleDashboardCardClick('open')}
